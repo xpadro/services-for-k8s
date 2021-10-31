c3acb629f0f0d1f4d8eaac91d94ffe589e716119
xpadro <xpadro.sw@gmail.com>
2021-10-31 09:50:52 +0100

    initial commit

---
+++ src/main/java/net/xpadro/services/k8s/ServicesForK8sApplication.java
@@ -0,0 +1,13 @@
+package net.xpadro.services.k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesForK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesForK8sApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/xpadro/services/k8s/ServicesForK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package net.xpadro.services.k8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesForK8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
